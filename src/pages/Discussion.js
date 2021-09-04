015643f3666d6e069c100c648e2bc9e0d2ac3310
RistoLibera <qofgxnm7@gmail.com>
2021-09-04 22:18:45 +0900

    Update one discussion page template

--- src/pages/Discussion.js
+++ src/pages/Discussion.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { AuthContext } from '../components/loading/Auth';
+import FirebasePack from '../config/FirebasePack';
+import { css } from '@emotion/react';
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
+
+//  embed uid into reply and rate
 
 const Discussion = () => {
+  const { uid }  = useParams();
+  const { currentUser } = useContext(AuthContext);
+  const spinnerCSS = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  `;
+  const [pageLoading, setPageLoading] = useState(true);
 
-  // Search results
+
+  const fetchThisDiscussion = async () => {
+    setPageLoading(true);
+
+    setPageLoading(false);
+  };
+  
+  useEffect(() => {
+    fetchThisDiscussion();
+  },[]);
+
+
   return (
     <section className='search-result-page'>
-      <h1>One Discussion</h1>
+      {pageLoading 
+        ?
+          <div className='page-loader'>
+            <ClimbingBoxLoader color='#D5D736' css={spinnerCSS} size={50} />
+          </div>
+        :
+        <div className='discussions-container'>
+
+        </div>
+      }
     </section>
   );
 };
