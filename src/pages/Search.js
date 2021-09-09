8abcb9694239d18b2fd492fe960bd66c76aa267e
RistoLibera <qofgxnm7@gmail.com>
2021-09-09 20:59:02 +0900

    Update show old searchings

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import FirebasePack from '../config/FirebasePack';
 import '../styles/css/search.css';
 
 // Search results
 const Search = () => {
   const { keyword }  = useParams();
+  const [listTags, setListTags] = useState([]);
 
   const runLocalstorage = () => {
     if (localStorage.length === 0) {
@@ -14,21 +15,61 @@ const Search = () => {
       localStorage.setItem('search-history', JSON.stringify(searchHistory));
     } else {
       let searchHistory = JSON.parse(localStorage.getItem('search-history'));
-      searchHistory.push(keyword);
-      localStorage.setItem('search-history', JSON.stringify(searchHistory));
+      if (searchHistory.some((word) => word === keyword)) {
+        return;
+      } else if (searchHistory.length > 4) {
+        searchHistory.shift();
+        searchHistory.push(keyword);
+        localStorage.setItem('search-history', JSON.stringify(searchHistory));  
+      } else {
+        searchHistory.push(keyword);
+        localStorage.setItem('search-history', JSON.stringify(searchHistory));  
+      }
     }
-    console.log(localStorage);
+  };
+
+  const getOldSearch = () => {
+    let array = JSON.parse(localStorage.getItem('search-history'));
+    array.reverse();
+    return array;
+  };
+
+  // Make one list HTML tag
+  const makeList = (result, index) => {
+    return (
+      <li key={index}>
+        <Link to={"/search/" + result}>{result}</Link>
+      </li>
+    );
+  };
+
+  const createList = async () => {
+    runLocalstorage();
+    let array = getOldSearch();
+    let container = [];
+
+    array.forEach((result, index) => {
+      let list =  makeList(result, index);
+      container.push(list);
+    });
+    setListTags(container);
   };
 
 
   useEffect(() => {
-    runLocalstorage();
+    createList();
   }, [keyword]);
 
   return (
     <section className='search-result-page'>
       <div className='search-history'>
-        <h1>history</h1>
+        <ul>
+          {listTags.map((li) => {
+            return (
+              li
+            );
+          })}
+        </ul>
       </div>
 
       <div className="titles">
