fb64d175a8b1be9d0296eb6b70d4b7b6d9721de2
RistoLibera <qofgxnm7@gmail.com>
2021-10-15 19:13:21 +0900

    Fix i18n suspense

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-
+import { css } from '@emotion/react';
+import ClockLoader from 'react-spinners/ClockLoader';
 // import i18n (needs to be bundled ;)) 
 import './i18n';
 
+const spinnerCSS = css`
+display: block;
+position: absolute;
+top: 50%;
+right: 50%;
+transform: translate(50%, -50%);
+margin: 0 auto;
+border-color: red;
+`;
+
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<div className='loading'><h1>...Authenticating...</h1></div>}>
+    <Suspense fallback={
+      <div className='page-loader'>
+        <ClockLoader color='#8E5829' css={spinnerCSS} size={100} />
+      </div>
+    }>
       <Routes />
     </Suspense>
   </React.StrictMode>,
